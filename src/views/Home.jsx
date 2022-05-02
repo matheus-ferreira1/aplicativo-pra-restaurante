20a9182e0a129b5c1e1cd5c5a841767b6033e6df
Matheus Ferreira <matheus.fer33@gmail.com>
2022-05-01 22:59:01 -0300

    criação do layout home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
    return (
-      <div>Home</div>
+      <div className='home'>
+         <div className='home-left'>
+            <h2>Imper</h2>
+            <h2>dível</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget quam consequat dui tincidunt pretium a non diam.</p>
+            <div className="home-bottom">
+               <h3>$ 30</h3>
+               <button>Peça agora!</button>
+            </div>
+         </div>
+         <div className="home-right">
+            <img src="https://source.unsplash.com/E94j3rMcxlw" alt="Really tasty burger" />
+         </div>
+      </div>
    )
 }
 
